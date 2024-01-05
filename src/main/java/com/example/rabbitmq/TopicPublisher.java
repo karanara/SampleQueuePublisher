443f13dc608885c4d621d7f1d4786202e3ea7f34
karanara <karanara@mail.uc.edu>
2024-01-05 02:21:31 -0500

    topic consumer

---
+++ src/main/java/com/example/rabbitmq/TopicPublisher.java
@@ -0,0 +1,23 @@
+package com.example.rabbitmq;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+public class TopicPublisher {
+
+	public static void main(String[] args) throws IOException, TimeoutException {
+		// TODO Auto-generated method stub
+		ConnectionFactory factory=new ConnectionFactory();
+		Connection connection=factory.newConnection();
+		Channel channel=connection.createChannel();
+		String message = "This is a Mobile Consumer and AC Consumer";
+		channel.basicPublish("Topic-Exchange", "tv.mobile.ac", null, message.getBytes());
+		channel.close();
+		connection.close();
+	}
+
+}
